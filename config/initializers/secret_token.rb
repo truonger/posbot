8fef80337a884619bec39dbeead578dbbe4a0b84
Mike Truong <mike@loudcrow.com>
2012-11-20 22:05:44 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Posbot::Application.config.secret_token = 'dfa576e03a3d68f0353ef98e456999eabec1c2e1fbfda7aa8b9ecd80b5ee19549e353da280246b212b7400042b48ce76abc6d905d96aef7c6df7377919ba4d7f'
